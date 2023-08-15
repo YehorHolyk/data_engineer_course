f0beffb9603cb6051e3724c6368bc07aeed2dd0a
yehor.holyk <yanov2222@gmail.com>
2023-08-15 18:29:44 +0300

    process_sales pipeline: - implemented transfer data from bronze to silver

--- final_project/dags/process_sales.py
+++ final_project/dags/process_sales.py
@@ -15,7 +15,7 @@ DEFAULT_ARGS = {
 }
 
 dag = DAG(
-    dag_id="sales_pipeline",
+    dag_id="process_sales_pipeline",
     description="Ingest and process sales data",
     schedule_interval='0 7 * * *',
     start_date=dt.datetime(2022, 9, 1),
@@ -36,7 +36,7 @@ transfer_from_data_lake_to_raw = BigQueryInsertJobOperator(
     project_id='de-07-yehor-holyk',
     configuration={
         "query": {
-            "query": "{% include 'sql/insert_to_bronze.sql' %}",
+            "query": "{% include 'sql/insert_sales_to_bronze.sql' %}",
             "useLegacySql": False,
             "tableDefinitions": {
                 "sales_csv": sales_csv,
@@ -48,3 +48,22 @@ transfer_from_data_lake_to_raw = BigQueryInsertJobOperator(
         'project_id': "de-07-yehor-holyk"
     }
 )
+
+transfer_from_bronze_to_silver = BigQueryInsertJobOperator(
+    task_id='transfer_from_bronze_to_silver',
+    dag=dag,
+    gcp_conn_id='gcp_conn',
+    location='us-east1',
+    project_id='de-07-yehor-holyk',
+    configuration={
+        "query": {
+            "query": "{% include 'sql/transfer_sales_to_silver.sql' %}",
+            "useLegacySql": False,
+        }
+    },
+    params={
+        'project_id': "de-07-yehor-holyk"
+    }
+)
+
+transfer_from_data_lake_to_raw >> transfer_from_bronze_to_silver
